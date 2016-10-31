7f29ee586d1990a615d79756ff94095fb7bf4cd9
songkai-aapt <songkaiape@gmail.com>
2016-10-31 09:31:26 +0800

    Create 51-N-Queens.py

---
+++ 51-N-Queens.py
@@ -0,0 +1,19 @@
+# N-Queens Problem
+# Given an integer n, return all distinct solutions to the n-queens puzzle.
+
+Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space respectively.
+
+class Solution(object):
+    def solveNQueens(self,n):
+        def DFS(queens,xy_dif,xy_sum):
+            p=len(queens)
+            if p==n:
+                result.append(queens)
+                return None
+            for q in range(n):
+                if q not in queens and p-q not in xy_dif and p+q not in xy_sum:
+                    DFS(queens+[q],xy_dif+[p-q],xy_sum+[p+q])
+     result=[]
+     DFS([],[],[])
+     return [["."*i + "Q" + "."*(n-i-1) for i in sol] for sol in result]
+     
